0726a0c22b3d87c694b145ffa7acbc24f7a72619
sogo <sogonishi@gmail.com>
2025-02-01 11:38:23 +0900

    Initial Commit

---
+++ immersivePlayer/immersivePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  immersivePlayerApp.swift
+//  immersivePlayer
+//
+//  Created by Sogo Nishihara on 2025/02/01.
+//
+
+import SwiftUI
+
+@main
+struct immersivePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ immersivePlayerTests/immersivePlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  immersivePlayerTests.swift
+//  immersivePlayerTests
+//
+//  Created by Sogo Nishihara on 2025/02/01.
+//
+
+import Testing
+@testable import immersivePlayer
+
+struct immersivePlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
